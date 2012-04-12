883a4112a1a89eca455078146ccfee95b2070509
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2012-04-12 23:26:33 +0200

    Implement %a %A %b %B %p %x %X

--- date.js
+++ date.js
@@ -17,6 +17,10 @@ if (!Date.toLocaleFormat) {
 			return (String(str).length == 1) ? fill+String(str) : String(str);
 		}
 		return formatString
+			.replace('%a', ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'][this.getDay()])
+			.replace('%A', ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'][this.getDay()])
+			.replace('%b', ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'][this.getMonth()])
+			.replace('%B', ['January','February','March','April','May','June','July','August','September','October','November','December'][this.getMonth()])
 			.replace('%C', new String(this.getFullYear()).substr(0,2))
 			.replace('%d', extend(this.getDate()))
 			.replace('%D', extend(this.getMonth()+1)+'/'+extend(this.getDate())+'/'+new String(this.getFullYear()).substr(2))
@@ -28,10 +32,13 @@ if (!Date.toLocaleFormat) {
 			.replace('%m', extend(this.getMonth()+1))
 			.replace('%M', extend(this.getMinutes()))
 			.replace('%n', '\\n')
+			.replace('%p', ['AM','PM'][Math.floor(this.getHours()/12)])
 			.replace('%R', extend(this.getHours())+':'+extend(this.getMinutes()))
 			.replace('%S', extend(this.getSeconds()))
 			.replace('%t', '\\t')
 			.replace('%T', extend(this.getHours())+':'+extend(this.getMinutes())+':'+extend(this.getSeconds()))
+			.replace('%x', extend(this.getMonth()+1)+'/'+extend(this.getDate())+'/'+this.getFullYear())
+			.replace('%X', this.toLocaleTimeString())
 			.replace('%y', new String(this.getFullYear()).substr(2))
 			.replace('%Y', this.getFullYear())
 			.replace('%%', '%');
